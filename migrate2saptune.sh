5a40b4eda95f57fcbed9817e3e4157382768558e
roseswe <3810660+roseswe@users.noreply.github.com>
2020-02-21 13:24:06 +0100

    chg: onsite enhancements and small sanity checks added

--- migrate2saptune.sh
+++ migrate2saptune.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-# $Header: /home/cvs/AddSourceCode/suse/sap/migrate2saptune.sh,v 1.4 2019/11/20 12:05:23 ralph Exp $
+# $Header: /home/cvs/AddSourceCode/suse/sap/migrate2saptune.sh,v 1.5 2020/02/20 20:33:03 ralph Exp $
+# (c) 2019-2020 by Ralph Roth, ROSE SWE
 # ---------------------------------------------------------------------------
-# This script tries to migrate a host running saptune v1 to saptune v2, see: 
+# This script tries to migrate a host running saptune v1 to saptune v2, see:
 #      man 7 saptune
-# rr, 26.09.2019 - initial creation, send feedback to Ralph.Roth@suse.com
+# rr, 26.09.2019 - initial creation, send feedback to Ralph dot Roth at suse dot com
 # rr, 12.11.2019 - many fixes around CSI output etc. Sanity checks
 # rr, 18.11.2019 - CVS keywords, beautified the source
+# rr, 18.02.2020 - workaround for corrupted SAPTUNE_VERSION= in /etc/sysconfig/saptune
 # ---------------------------------------------------------------------------
 
 export LANG=posix
@@ -13,7 +15,10 @@ export LANG=posix
 # little sanity check: is saptune installed and are we root (/usr/sbin/saptune)
 which saptune || exit 42
 
-# needed later, maybe we need here a trap handling to remove this file ...
+# overdosing is not needed, means v2 already active
+saptune version | grep -q "version is '2'$"  && exit 41
+
+# needed later, maybe we need here a trap handling to remove this file ...?
 TMPFILE=$(mktemp)
 
 # we need this information later to apply them again
@@ -36,7 +41,8 @@ do
 done
 
 sed 's/SAPTUNE_VERSION="1"/SAPTUNE_VERSION="2"/' /etc/sysconfig/saptune > $TMPFILE
-mv -f $TMPFILE /etc/sysconfig/saptune
+mv -f $TMPFILE /etc/sysconfig/saptune   ## -rw-r--r--, 644
+chmod 644 /etc/sysconfig/saptune        ## rpm -V, chkstat(8)
 
 echo "### Please check if the following variables in /etc/sysconfig/saptune are empty and SAPTUNE_VERSION=2 is set"
 egrep -v "^#|^$" /etc/sysconfig/saptune
@@ -55,7 +61,7 @@ mv -f $TMPFILE /etc/security/limits.conf
 
 systemctl restart tuned.service || echo "#####  ---  ERROR restarting tuned  ---  #####"
 
-# sanity, seen on a few hosts, not sure if this is a bug of 2.0.1-3.16.1
+# sanity, seen on a few hosts, not sure if this is a bug of 2.0.1-3.16.1, saptune-2.0.2-3.21.1.x86_64
 grep -q "SAPTUNE_VERSION=" /etc/sysconfig/saptune || echo 'SAPTUNE_VERSION="2"' >> /etc/sysconfig/saptune
 
 # ---------------------------------------------------------------------------
@@ -76,6 +82,9 @@ do
     saptune note verify $f
 done
 
+# sanity, seen on a few hosts, not sure if this is a bug of 2.0.1-3.16.1, saptune-2.0.2-3.21.1.x86_64
+grep -q "SAPTUNE_VERSION=" /etc/sysconfig/saptune || echo 'SAPTUNE_VERSION="2"' >> /etc/sysconfig/saptune
+rpm -q saptune
 saptune version
 RET=$?
 exit $RET
